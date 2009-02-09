42c9d8c4e0ed46f77ecc5db0cf2bb94e2b410c59
aglenn <aaron@aaronglenn.ca>
2009-02-09 15:32:55 -0700

    Adding 404 template. Will render alternate layout if in path.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,28 @@
 class PagesController < ApplicationController
-
+  
+  layout :determine_layout
+  
+  # Last Resort For Page = Static Page view
   def method_missing(method)
     render :template => '/pages/'+params[:path].join('/'), :use_layout => true if params[:path]
     cache_page
   end
+  
+  # Will Rescue Errors like 404
+  def rescue_action(exception)
+    @exception = exception
+    render :template => 'errors/404', :layout => 'errors'
+  end
+  
+  protected
+  
+    # Runs down the path (from end to start) looking for layout file
+    def determine_layout 
+        params[:path].reverse.each do |segment|
+          @alternate_layout ||= segment if File.exist?(RAILS_ROOT+'/app/views/layouts/'+segment+'.html.haml')
+          @alternate_layout ||= segment if File.exist?(RAILS_ROOT+'/app/views/layouts/'+segment+'.html.erb')
+        end
+        @alternate_layout.to_s || 'application'
+    end
 
 end
