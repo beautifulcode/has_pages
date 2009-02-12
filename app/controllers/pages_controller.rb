19c9f5f41a0c89df45f72e4311ae4c038abd66d5
aglenn <aaron@aaronglenn.ca>
2009-02-12 12:18:46 -0700

    Adding error pages. Fixing layout code. Adding default layouts.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,27 +4,38 @@ class PagesController < ApplicationController
   
   # Last Resort For Page = Static Page view
   def method_missing(method)
-    view_path = params[:path].join('/')
-    view_path = 'index' if view_path.blank?
-    render :template => '/pages/'+view_path
+    page_path = params[:path].join('/')
+    page_path = 'index' if page_path.blank?
+    render :template => '/pages/'+page_path
     cache_page
   end
   
-  # Will Rescue Errors like 404
+  # Will Rescue Errors like 404/500
   def rescue_action(exception)
     @exception = exception
-    render :template => 'errors/404', :layout => 'errors'
+    if @exception.class == ActionView::MissingTemplate
+      render :template => 'errors/404', :layout => 'errors' 
+    else
+      # render :template => 'errors/500', :layout => 'errors'
+      # Allow the default rails error. It is much more helpful.
+    end
   end
   
   protected  
   
     # Runs down the path (from end to start) looking for layout file
-    def determine_layout 
-        params[:path].reverse.each do |segment|
-          @alternate_layout ||= segment if File.exist?(RAILS_ROOT+'/app/views/layouts/'+segment+'.html.haml')
-          @alternate_layout ||= segment if File.exist?(RAILS_ROOT+'/app/views/layouts/'+segment+'.html.erb')
+    def determine_layout
+        if params[:path].empty?
+          @page_layout = 'home' if File.exist?(RAILS_ROOT+'/app/views/layouts/home.html.haml')
+          @page_layout = 'home' if File.exist?(RAILS_ROOT+'/app/views/layouts/home.html.erb')
+        else
+          params[:path].reverse.each do |segment|
+            @alternate_layout ||= segment if File.exist?(RAILS_ROOT+'/app/views/layouts/'+segment+'.html.haml')
+            @alternate_layout ||= segment if File.exist?(RAILS_ROOT+'/app/views/layouts/'+segment+'.html.erb')
+          end
+          @page_layout = @alternate_layout.to_s 
         end
-        @alternate_layout.to_s || 'application'
+        @page_layout || 'application'
     end
 
 end
